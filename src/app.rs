a6818f4a8fd026ab47955c7c1a4567a56f1a8fb8
Sergio Rubio <srubio@jabawaka.com>
2025-04-22 10:50:11 +0200

    Moved to egui render, basic entries rendering

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,8 @@
 use std::cmp::Ordering;
 use std::fs;
-use time::{Date, OffsetDateTime};
+
+use eframe::egui;
+use time::{Date, OffsetDateTime, format_description};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -9,87 +11,6 @@ pub struct Entry {
     pub weight_kg: Option<f32>,
     pub waist_cm: Option<f32>,
     pub date: Date,
-}
-
-pub struct EditString {
-    pub prev: String,
-    pub next: String,
-}
-
-impl EditString {
-    pub fn new() -> EditString {
-        EditString {
-            prev: String::new(),
-            next: String::new()
-        }
-    }
-
-    pub fn from(string: String, cursor_pos: usize) -> EditString {
-        if cursor_pos == 0 {
-            // This means to put the cursor at the end of the string
-            EditString {
-                prev: string.clone(),
-                next: String::new()
-            }
-        } else {
-            if cursor_pos > string.len() {
-                EditString {
-                    prev: string.clone(),
-                    next: String::new()
-                }
-            } else {
-                let (prev, next) = string.split_at(cursor_pos);
-                EditString {
-                    prev: String::from(prev),
-                    next: String::from(next)
-                }
-            }
-        }
-    }
-
-    pub fn add_char(&mut self, c: char) {
-        self.prev.push(c);
-    }
-
-    pub fn remove_char(&mut self) {
-        if self.prev.len() > 0 {
-            self.prev.pop();
-        }
-    }
-
-    pub fn next_cursor(&mut self) {
-        if self.next.len() > 0 {
-            let char = self.next.remove(0);
-            self.prev.push(char);
-        }
-    }
-
-    pub fn prev_cursor(&mut self) {
-        if self.prev.len() > 0 {
-            let char = self.prev.pop().unwrap();
-            self.next.insert(0, char);
-        }
-    }
-
-    pub fn to_final_string(&self) -> String {
-        let mut final_string = String::from(&self.prev);
-        final_string.push_str(&self.next);
-
-        final_string
-    }
-
-    pub fn to_edit_string(&self) -> String {
-        let mut edit_string = String::from(&self.prev);
-        edit_string.push('_');
-
-        let mut next_str = String::from(&self.next);
-        if next_str.len() > 0 {
-            next_str.remove(0);
-        }
-        edit_string.push_str(&next_str);
-
-        edit_string
-    }
 }
 
 pub enum CurrScreen {
@@ -114,29 +35,26 @@ pub enum ZoomLevel {
 
 pub const GRAPH_POINTS: u8 = 8;
 
-pub struct App {
+pub struct MyApp {
     pub entries: Vec<Entry>,
-    pub edit_content: EditString,
-    pub edit_weight: EditString,
-    pub edit_waist: EditString,
     pub curr_date: Date,
     pub curr_screen: CurrScreen,
     pub edit_value: EditValue,
     pub zoom: ZoomLevel,
 }
 
-impl App {
-    pub fn new() -> App {
-        App {
+impl MyApp {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        let mut app = MyApp {
             entries: vec![],
-            edit_content: EditString::new(),
-            edit_weight: EditString::new(),
-            edit_waist: EditString::new(),
             curr_date: OffsetDateTime::now_local().unwrap().date(),
             curr_screen: CurrScreen::Main,
             edit_value: EditValue::Content,
             zoom: ZoomLevel::Day,
-        }
+        };
+        app.load_from_file();
+
+        app
     }
 
     pub fn save_to_file(&self) {
@@ -147,112 +65,6 @@ impl App {
     pub fn load_from_file(&mut self) {
         if let Ok(file_contents) = fs::read_to_string("diary.json") {
             self.entries = serde_json::from_str(&file_contents).unwrap();
-        }
-    }
-
-    pub fn enter_edit_mode(&mut self) {
-        self.curr_screen = CurrScreen::Editing;
-        self.edit_value = EditValue::Content;
-
-        if let Some(entry) = self.get_entry_by_date(self.curr_date) {
-            self.edit_content = EditString::from(entry.content, 0);
-
-            if let Some(weight) = entry.weight_kg {
-                self.edit_weight = EditString::from(weight.to_string(), 0);
-            } else {
-                self.edit_weight = EditString::new();
-            }
-
-            if let Some(waist) = entry.waist_cm {
-                self.edit_waist = EditString::from(waist.to_string(), 0);
-            } else {
-                self.edit_waist = EditString::new();
-            }
-        } else {
-            self.edit_content = EditString::new();
-            self.edit_weight = EditString::new();
-            self.edit_waist = EditString::new();
-        }
-    }
-
-    pub fn enter_main_mode(&mut self) {
-        self.curr_screen = CurrScreen::Main;
-
-        let content = self.edit_content.to_final_string();
-
-        let final_weight_kg;
-        if let Ok(weight_kg) = self.edit_weight.to_final_string().parse::<f32>() {
-            final_weight_kg = Some(weight_kg);
-        } else {
-            final_weight_kg = None;
-        }
-
-        let final_waist_cm;
-        if let Ok(waist_cm) = self.edit_waist.to_final_string().parse::<f32>() {
-            final_waist_cm = Some(waist_cm);
-        } else {
-            final_waist_cm = None;
-        }
-
-        if content.len() > 0 || final_weight_kg != None || final_waist_cm != None {
-            let entry_to_save = Entry {
-                content: content,
-                weight_kg: final_weight_kg,
-                waist_cm: final_waist_cm,
-                date: self.curr_date
-            };
-
-            self.save_entry(entry_to_save);
-        }
-    }
-
-    pub fn type_char(&mut self, c: char) {
-        match self.edit_value {
-            EditValue::Content => {
-                self.edit_content.add_char(c);
-            }
-            EditValue::Weight => {
-                if c.is_digit(10) || c == '.' {
-                    self.edit_weight.add_char(c);
-                }
-            }
-            EditValue::Waist => {
-                if c.is_digit(10) || c == '.' {
-                    self.edit_waist.add_char(c);
-                }
-            }
-        }
-    }
-
-    pub fn remove_char(&mut self) {
-        match self.edit_value {
-            EditValue::Content => self.edit_content.remove_char(),
-            EditValue::Weight  => self.edit_weight.remove_char(),
-            EditValue::Waist   => self.edit_waist.remove_char(),
-        }
-    }
-
-    pub fn cycle_edit_value(&mut self) {
-        match self.edit_value {
-            EditValue::Content => self.edit_value = EditValue::Weight,
-            EditValue::Weight  => self.edit_value = EditValue::Waist,
-            EditValue::Waist   => self.edit_value = EditValue::Content,
-        }
-    }
-
-    pub fn next_cursor(&mut self) {
-        match self.edit_value {
-            EditValue::Content => self.edit_content.next_cursor(),
-            EditValue::Weight  => self.edit_weight.next_cursor(),
-            EditValue::Waist   => self.edit_waist.next_cursor(),
-        }
-    }
-
-    pub fn prev_cursor(&mut self) {
-        match self.edit_value {
-            EditValue::Content => self.edit_content.prev_cursor(),
-            EditValue::Weight  => self.edit_weight.prev_cursor(),
-            EditValue::Waist   => self.edit_waist.prev_cursor(),
         }
     }
 
@@ -430,5 +242,33 @@ impl App {
         }
 
         waists
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Variables used in all layouts
+        let weight_vec: Vec<f32>;
+        let waist_vec: Vec<f32>;
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical(|ui| {
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    // Section with diary entries
+                    for entry in &mut self.entries {
+                        let format = format_description::parse("[day]-[month]-[year]").unwrap();
+                        let date_string = entry.date.format(&format).unwrap();
+
+                        if entry.content.len() > 0 {
+                            ui.heading(date_string);
+                            ui.label(&entry.content);
+                            ui.add_space(10.0);
+                        }
+                    }
+
+                    // Section with graphs
+                });
+            });
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,11 @@
 use std::{error::Error, io};
 
-use crossterm::event;
-use ratatui::{
-    backend::{Backend, CrosstermBackend},
-    crossterm::{
-        event::{Event, KeyCode, EnableMouseCapture, DisableMouseCapture},
-        execute,
-        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
-    },
-    Terminal,
-};
-
-
 mod app;
-mod ui;
 
-use crate::{
-    app::{App, CurrScreen},
-    ui::ui,
-};
+use crate::app::MyApp;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // Setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+fn main() {
+    let native_options = eframe::NativeOptions::default();
 
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    // Create app and run it
-    let mut app = App::new();
-    app.load_from_file();
-    let _ = run_app(&mut terminal, &mut app);
-
-    // Clean up terminal
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
-    terminal.show_cursor()?;
-
-    Ok(())
-}
-
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
-    loop {
-        // Draw the app
-        terminal.draw(|f| ui(f, app))?;
-
-        // Handle input
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Release {
-                // Skip events that are not KeyEventKind::Press
-                continue;
-            }
-
-            match app.curr_screen {
-                CurrScreen::Main => match key.code {
-                    KeyCode::Esc => break,
-                    KeyCode::Char('e') => app.enter_edit_mode(),
-                    KeyCode::Char('h') | KeyCode::Left  => app.curr_date = app.curr_date.previous_day().unwrap(),
-                    KeyCode::Char('l') | KeyCode::Right => app.curr_date = app.curr_date.next_day().unwrap(),
-                    KeyCode::Char('j') | KeyCode::Up    => app.curr_date = app.curr_date.prev_occurrence(app.curr_date.weekday()),
-                    KeyCode::Char('k') | KeyCode::Down  => app.curr_date = app.curr_date.next_occurrence(app.curr_date.weekday()),
-                    KeyCode::Char('>') => app.next_zoom(),
-                    KeyCode::Char('<') => app.prev_zoom(),
-                    _ => {},
-                }
-                CurrScreen::Editing => match key.code {
-                    KeyCode::Esc => app.curr_screen = CurrScreen::DiscardChanges,
-                    KeyCode::Enter => {app.enter_main_mode(); app.save_to_file();},
-                    KeyCode::Char(c) => app.type_char(c),
-                    KeyCode::Backspace => app.remove_char(),
-                    KeyCode::Tab => app.cycle_edit_value(),
-                    KeyCode::Left => app.prev_cursor(),
-                    KeyCode::Right => app.next_cursor(),
-                    _ => {},
-                }
-                CurrScreen::DiscardChanges => match key.code {
-                    KeyCode::Char('y') | KeyCode::Char('Y') => app.curr_screen = CurrScreen::Main,
-                    KeyCode::Char('n') | KeyCode::Char('N')=> app.curr_screen = CurrScreen::Editing,
-                    _ => {},
-                }
-            }
-        }
-    }
-
-    Ok(true)
+    let _ = eframe::run_native("Diary",  native_options, Box::new(|cc| Ok(Box::new(MyApp::new(cc)))));
 }
